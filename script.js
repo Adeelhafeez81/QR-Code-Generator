f0880e55532d3782b5e50ef42f94f01402a6e357
Adeelhafeez81 <142439928+Adeelhafeez81@users.noreply.github.com>
2024-03-19 19:51:50 -0700

    QR Code Generator

---
+++ script.js
@@ -0,0 +1,10 @@
+let qrText = document.querySelector("#qrText");
+let imageBox = document.querySelector("#imageBox");
+let qrImage = document.querySelector("#qrImage");
+
+function qrGenerator() {
+  qrImage.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150*150&data=" +
+    qrText.value;
+  imageBox.classList.add("show-image");
+}
